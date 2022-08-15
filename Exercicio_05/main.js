10d5e5032d85febbea53a072aa94e81350cdd264
Vinicius Victor <viniciuslimavi05@gmail.com>
2022-08-15 09:00:04 -0300

    Adicionando funcionalidades

--- Exercicio_05/main.js
+++ Exercicio_05/main.js
@@ -2,7 +2,7 @@ let vet = [];
 let num = document.querySelector("input#num");
 let botao = document.querySelector("input#botao");
 let lista = document.querySelector("select#lista");
-let res = document.querySelector("res");
+let res = document.querySelector("div#res");
 
 function adicionarNum(n) {
   if (Number(n) >= 1 && Number(n) <= 100) {
@@ -13,7 +13,7 @@ function adicionarNum(n) {
 }
 
 function verifiLista(n, l) {
-  if (l.indexOf(Number(n) != -1)) {
+  if (l.indexOf(Number(n)) != -1) {
     return true;
   } else {
     return false;
@@ -22,29 +22,34 @@ function verifiLista(n, l) {
 
 function adicionar() {
   if (adicionarNum(num.value) && !verifiLista(num.value, vet)) {
-    window.alert("tudo Ok");
+    vet.push(Number(num.value));
+    let item = document.createElement("option");
+    item.text = `Valor ${num.value} adicionado`;
+    lista.appendChild(item);
+    res.innerHTML = "";
   } else {
-    window.alert("Tente novamente");
+    window.alert("Valor inválido ou já encontrado na lista");
   }
-}
-
-/*function adicionar() {
-  if (num >= 1 && num <= 100) {
-    if (vet.indexOf(num) != num) {
-      vet.push(num);
-      lista.innerHTML += `O número ${num} adicionado.`;
-    } else if (num == 0) {
-      window.alert("Por favor, digite um número!");
-    } else {
-      window.alert("Este número ja existe");
-    }
-  }
+  num.value = "";
+  num.focus(); // O input de número no site passa a coletar automaticamente sem precisar selecionar.
 }
 
 function finalizador() {
-  for (let cont = 0; cont <= vet; cont++) {
-    res.innerHTML += `Foi adicionado ${vet} números.`;
+  if (vet.length == 0) {
+    window.alert("Adicione alguns números a sua lista!");
+  } else {
+    let total = vet.length;
+    let maior = vet[0];
+    let menor = vet[0];
+    for (let pos in vet) {
+      if (vet[pos] > maior) {
+        maior = vet[pos];
+      } else if (vet[pos] < menor) {
+        menor = vet[pos];
+      }
+    }
+    res.innerHTML += `No total temos ${total} números cadastrados`;
+    res.innerHTMl += `O maior número informado é ${maior}`;
+    res.innerHTML += `O menor número informado é ${menor}`;
   }
 }
-
-function somar() {}*/
